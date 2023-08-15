0418cec094b2f41701d854b89cb432e636fab15a
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-15 15:45:17 -0500

    Agregando la barra de navegacion

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,11 @@
 import AppRouter from './router/AppRouter'
 
+
 const HeroesApp = () => {
   return (
     <>
-      <AppRouter/>
-      <h1>Heroes-App</h1>
+      <AppRouter />
+
     </>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import MarvelPage from '../heroes/pages/MarvelPage'
 import LoginPage from '../auth/pages/LoginPage'
 import DcPages from '../heroes/pages/DcPages'
+import NavBar from '../ui/components/NavBar'
 
 const AppRouter = () => {
   return (
     <>
+      <NavBar/>
+      
       <Routes>
         <Route path="marvel" element={<MarvelPage/>} />
         <Route path="dc" element={<DcPages/>} />
