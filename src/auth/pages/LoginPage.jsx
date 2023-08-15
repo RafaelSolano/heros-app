303644e0470bb1a43382be94019a00ee066e3a3d
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-15 17:45:40 -0500

    194. useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,17 +3,19 @@ import { useNavigate } from 'react-router-dom'
 const LoginPage = () => {
   const navigate = useNavigate();
 
-  const onLogout = () => {
+  const onLogin = () => {
     navigate('/',{replace: true})
   }
   
   return (
     <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr/>
 
       <button
         className="btn btn-primary"
-        onClick={onLogout}
-      >Logout</button>
+        onClick={onLogin}
+      >Login</button>
       
     </div>
   )
