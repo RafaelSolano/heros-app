531e2fb31c20eb86b46d2ed8611e1b9c5f9884d9
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-15 17:35:18 -0500

    193

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/',{replace: true})
+  }
+  
   return (
-    <div>LoginPage</div>
+    <div className='container mt-5'>
+
+      <button
+        className="btn btn-primary"
+        onClick={onLogout}
+      >Logout</button>
+      
+    </div>
   )
 }
 
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,6 +1,11 @@
+import { HeroList } from '../components/HeroList'
+
 const MarvelPage = () => {
   return (
-    <div>MarvelPage</div>
+    <div>
+      <h1>Marvel</h1>
+      {/* <HeroList publisher={'marvel'}/> */}
+    </div>
   )
 }
 
