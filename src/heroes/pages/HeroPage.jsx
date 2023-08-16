6f2fbe2b7459cbedb7f75a10b010d6abddabcf6b
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-15 20:58:23 -0500

    199 se crea la paga Heroe

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,11 +1,13 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import getHeroById from '../helpers/getHeroById';
 
 const HeroPage = () => {
+  const navigate = useNavigate();
+
   const { heroId } =useParams();
   console.log(heroId);
 
-  const hero = getHeroById(heroId);
+  const  hero= getHeroById(heroId);
   console.log(hero);
 
   if (!hero) {
@@ -13,8 +15,34 @@ const HeroPage = () => {
     
   }
 
+  const handleNavigateReturn = () => {
+    navigate(-1)
+  }
+  
+
   return (
-    <div>HeroPage</div>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          className='img-thumbnail'
+          src={`/assets/${heroId}.jpg`}
+          alt={hero.superhero} />
+      </div>
+      <div className='col-8'>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego:</b> {hero.alter_ego}</li>
+          <li className='list-group-item'><b>Publisher:</b> {hero.publisher}</li>
+          <li className='list-group-item'><b>First apperarance:</b> {hero.first_appearance}</li>
+        </ul>
+        <h5 className='mt3'> Characters</h5>
+        <p>{hero.characters}</p>
+        <button className=' btn  btn-dark ' onClick={handleNavigateReturn}>
+          Back
+        </button>
+
+      </div>
+
+    </div>
   )
 }
 
